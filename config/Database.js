73419bed9fd94c2da6bb714eee81d67fdd70627c
Ilya Martinkevich <ilia1mail2martinkevi4@gmail.com>
2020-04-22 11:02:49 +0300

    Sequelize

--- config/Database.js
+++ config/Database.js
@@ -1,36 +1,82 @@
-var mysql = require('mysql');
+/*
+var mysql = require('mysql2');
 
 //local mysql db connection
 
-var db_config = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'notes'
-};
-let connection;
+let connection = mysql.createPool({
+    connectionLimit: 5,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "notes"
+});
 
-function handleDisconnect() {
-    connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                     // the old one cannot be reused.
+console.log("connect User");
+module.exports = connection;*/
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("notes", "root", "", {
+    dialect: "mysql",
+    host: "localhost",
+    define: {
+        timestamps: false
+    }
+});
 
-    connection.connect(function(err) {              // The server is either down
-        if(err) {                                     // or restarting (takes a while sometimes).
-            console.log('error when connecting to db:', err);
-             setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-        }                                     // to avoid a hot loop, and to allow our node script to
-    });                                     // process asynchronous requests in the meantime.
-                                            // If you're also serving http, display a 503 error.
-    connection.on('error', function(err) {
-        console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-            handleDisconnect();                         // lost due to either server restart, or a
-        } else {                                      // connnection idle timeout (the wait_timeout
-            throw err;                                  // server variable configures this)
-        }
-    });
-}
+const User = sequelize.define("user", {
+    login: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }    ,
+    cookie: {
+        type: Sequelize.STRING,
+    }
+});
 
-handleDisconnect();
+const Notes = sequelize.define("notes", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement:true,
+        primaryKey: true,
+        allowNull: false
+    },
+    login: {
+        foreignKey: true,
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    theme: {
+        type: Sequelize.STRING,
+    }    ,
+    message: {
+        type: Sequelize.STRING,
+    },
+    dateCreate: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    dateUpdate: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+});
+User.hasMany(Notes, {foreignKey: 'login'});
 
-module.exports = connection;
+sequelize.sync().then(result => console.log("result"))
+    .catch(err => console.log(err));
+
+/*User.create({
+    name: "Tom",
+    age: 35
+}).then(res => {
+    console.log(res);
+}).catch(err => console.log(err));*/
+//module.exports = connection;
--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const port = process.env.PORT || 3005;
 
 const server = http.createServer(app);
 
-server.listen(port,()=>console.log("Express server is running at port no : "+port));
+server.listen(port,()=>console.log("Express server is running at port no http://127.0.0.1:"+port));
