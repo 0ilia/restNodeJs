c2978dfca2c34a967394db3e1bcb37cc7e4034df
Ilya Martinkevich <ilia1mail2martinkevi4@gmail.com>
2020-04-22 13:43:21 +0300

    new file DB

--- config/Database.js
+++ config/Database.js
@@ -1,36 +1,11 @@
-var mysql = require('mysql');
+const mysql = require("mysql2");
 
-//local mysql db connection
-
-var db_config = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'notes'
-};
-let connection;
-
-function handleDisconnect() {
-    connection = mysql.createConnection(db_config); // Recreate the connection, since
-    // the old one cannot be reused.
-
-    connection.connect(function(err) {              // The server is either down
-        if(err) {                                     // or restarting (takes a while sometimes).
-            console.log('error when connecting to db:', err);
-            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-        }                                     // to avoid a hot loop, and to allow our node script to
-    });                                     // process asynchronous requests in the meantime.
-                                            // If you're also serving http, display a 503 error.
-    connection.on('error', function(err) {
-        console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-            handleDisconnect();                         // lost due to either server restart, or a
-        } else {                                      // connnection idle timeout (the wait_timeout
-            throw err;                                  // server variable configures this)
-        }
-    });
-}
-
-handleDisconnect();
+const connection = mysql.createPool({
+    connectionLimit: 5,
+    host: "localhost",
+    user: "root",
+    password: "пароль_от_сервера_mysql",
+    database: "usersdb"
+});
 
 module.exports = connection;
